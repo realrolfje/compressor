bf1eb2e86b9a90e863d0329ef06fd59c0987d02a
Rolf <no-reply@example.com>
2015-11-17 21:09:27 +0100

    Initial commit and setup.

---
+++ src/compressor/compressor.ino
@@ -0,0 +1,27 @@
+/*
+ * Arduino based audio compressor
+ */
+
+
+const int inputPin = 3;
+const int outputPin = 5;
+
+const int buffersize = 100;
+int samplebuffer[buffersize];
+int headpointer=0;
+int tailpointer=buffersize-1;
+
+void setup() {
+  // Init buffer
+  for (int i=0; i < buffersize; i++){
+    samplebuffer[i]=0;
+  } 
+}
+
+void loop() {
+  samplebuffer[headpointer] = analogRead(inputPin);
+  analogWrite(outputPin, samplebuffer[tailpointer]);
+
+  headpointer = (headpointer++) mod buffersize;
+  tailpointer = (tailpointer++) mod buffersize;
+}
