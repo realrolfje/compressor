c670233e0fc6a9e007c8a2aafc3f53fb25b3ab03
Rolf <no-reply@example.com>
2015-11-19 23:40:23 +0100

    More runloop optimizations

--- src/compressor/compressor.ino
+++ src/compressor/compressor.ino
@@ -12,26 +12,40 @@ const float targettop = 150.0;
 const float maxgain = 1.0 * (256/1024); // output is 8 bit, input is 10 bit.
 const int clippinglevel = 500;
 
-float oldtopfiltered = 0.0;
-float newtopfiltered = 0.0;
-boolean clockboolean = false;
-boolean clippingboolean = false;
+// Ringbuffer
+const int buffersize = attack;
+int ring[buffersize];
 
 void setup() {
-  ringinit();
+  // Initialize ring buffer
+  for (int i=0; i<buffersize; i++) {
+    ring[i]=0;
+  }
+
   changePrescaler();
   pinMode(clockoutpin, OUTPUT);
   pinMode(clippingledpin, OUTPUT); 
 }
 
 void loop() {
+  // Initializing variables inside the function
+  // increases memory access performance.
+  float oldtopfiltered = 0.0;
+  float newtopfiltered = 0.0;
+  boolean clockboolean = false;
+  boolean clippingboolean = false;
+  int ringpointer=0;
+  
   // While(true) prevents the code from leaving the loop()
   // function and re-entering it, with all the stack manipulation
   // that comes with it.
   while(true) {
-    // Get sample and topdetector
+    // Get audio sample
     int sample = analogRead(inputPin);
-    ringstore(sample);
+
+    // Store sample in the ringbuffer
+    ringpointer = (ringpointer+1) % buffersize;
+    ring[ringpointer] = sample;
     
     // Subtract DC offset and full-wave rectify
     int topped = abs(512-sample);
@@ -47,10 +61,13 @@ void loop() {
     
     // Apply calclated gain to a sample which is precisely the
     // number of samples ago as the attack time to prevent plopping.
-    int output = round(gain * ringfetch());
+    int tailpointer = (ringpointer + buffersize - 1) % buffersize;
+    int output = round(gain * ring[tailpointer]);
     analogWrite(outputPin, output);
     
-    // Flip a pin to show our clockspeed
+    // Flip a pin to show our clockspeed.
+    // Outputpin should oscilate at 8.078 kHz,
+    // meaning we have a sample rate of twice that.
     clockboolean = !clockboolean;
     digitalWrite(clockoutpin, clockboolean);
     
